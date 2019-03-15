a54977d53147a83ae92b0408dc4eb0725d55caf7
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-03-15 13:34:33 +0100

    more content,

--- src/art/SocialMediaCalendar.hx
+++ src/art/SocialMediaCalendar.hx
@@ -10,7 +10,7 @@ class SocialMediaCalendar {
 	static public var arr:Array<SocialMediaObj> = [
 		{
 			title: "MEMES",
-			description: "Because who doesn’t love a good meme? Bonus points if it’s relevant to your niche. (I’m a nerd because social media memes are my life.)"
+			description: "Because who doesn’t love a good meme? Bonus points if it’s relevant to your niche."
 		},
 		{
 			title: "QUOTES",
@@ -26,7 +26,7 @@ class SocialMediaCalendar {
 		},
 		{
 			title: "CALLS TO ACTION",
-			description: "Get followers to sign up for your email newsletter, read your new blog post, or buy your newest product/ service"
+			description: "Get followers to sign up for your email newsletter, read your new blog post, or buy your newest product /  service"
 		},
 		{
 			title: "FREEBIES",
@@ -34,7 +34,7 @@ class SocialMediaCalendar {
 		},
 		{
 			title: "QUESTION OF THE DAY",
-			description: "Ask your followers a question about their blog/biz, advice for your blog/biz, or just something fun."
+			description: "Ask your followers a question about their blog / biz, advice for your blog / biz, or just something fun."
 		},
 		{
 			title: "VIDEOS",
@@ -42,7 +42,7 @@ class SocialMediaCalendar {
 		},
 		{
 			title: "DISCOUNTS",
-			description: "Discounts/offers on products/services that you offer"
+			description: "Discounts / offers on products / services that you offer"
 		},
 		{
 			title: "CURRENT EVENTS",
@@ -50,7 +50,7 @@ class SocialMediaCalendar {
 		},
 		{
 			title: "READER SURVEYS",
-			description: "Get follower input on your blog/biz"
+			description: "Get follower input on your blog / biz"
 		},
 		{
 			title: "VIRTUAL EVENTS",
@@ -58,7 +58,7 @@ class SocialMediaCalendar {
 		},
 		{
 			title: "QUICK TIPS",
-			description: "Secret tips/hacks for getting things done"
+			description: "Secret tips / hacks for getting things done"
 		},
 		{
 			title: "ANNOUNCEMENTS",
@@ -74,7 +74,7 @@ class SocialMediaCalendar {
 		},
 		{
 			title: "GIVEAWAYS",
-			description: "Give away prizes in exchange for likes/ followers"
+			description: "Give away prizes in exchange for likes /  followers"
 		},
 		{
 			title: "LONGER ANECDOTES",
@@ -98,7 +98,7 @@ class SocialMediaCalendar {
 		},
 		{
 			title: "PRODUCTS / SERVICES",
-			description: "Share info about new products/services that you’re offering"
+			description: "Share info about new products / services that you’re offering"
 		},
 		{
 			title: "INSPIRATION",
@@ -118,7 +118,7 @@ class SocialMediaCalendar {
 		},
 		{
 			title: "SOCIAL-ONLY PROMOS",
-			description: "Discounts/offers on your products/services that are only for your social media followers"
+			description: "Discounts / offers on your products / services that are only for your social media followers"
 		},
 		{
 			title: "PERSONALITY",
@@ -126,7 +126,7 @@ class SocialMediaCalendar {
 		},
 		{
 			title: "TIMESENSITIVE OFFERS",
-			description: "Discounts/offers that your followers have to use by a certain date before they expire"
+			description: "Discounts / offers that your followers have to use by a certain date before they expire"
 		},
 		{
 			title: "",
@@ -137,6 +137,71 @@ class SocialMediaCalendar {
 	public function new() {
 		var temp = [
 
+"
+		Workspace,
+		Piece from portfolio,
+		What are you working on?
+		One thing you can't live without,
+		Ho do you wrap up your week?,
+		What do you do on Saturdays?,
+		What inspires you?,
+		Motivation Monday,
+		Share a recent blog post
+		Share a time you were pushed out of you comfort zone
+		Snapshot of daily routine
+		Promote upcoming e-course, webinar, of email list
+		Share your favorite life hack
+		Share a fun fact about you,
+		Inspiring quote
+		Show part of your portfolio
+		Show a alternative work space
+		Something that you recently splurged on
+		Quick tip
+		Friday Introductions
+		A corner in you home that refreshes
+		Something in your office that inspires you
+		Show of your brand
+		How did you come up with your biz name
+		Show a personal project you're working on
+		Your favorite protfolio project this month
+		Introduce your pets and/or kids
+		Where are you from?
+		Show off one of your clients
+		Your favorite business resource
+		Promote a package or product you offer
+
+
+		Share a post
+		Inspirational quote
+		A behind the scenes/day in the life photo
+		Offer a quick tip
+		Share one of your favorite books
+		Share a fill in the blank post
+		Client testimonial/feedback
+		A picture of a product that you offer
+		Share a candid photo of you or your team
+		Post a \"sneak peek\" of teaser Content
+		Promote a free download that you offer
+		Ask a question
+		Share a favorite resource of tool you use
+		A short video clip
+		Hightlight a beneficial blog post from someone else
+		Recommend a favorite product
+		Share on of your favorite websites of blogs
+		tag a friend of someone that you admire
+		share a service or product that you offer
+		invite community sign up for your newsletter
+		Share an inspirational story of perserverance
+		post a pi of your workspace
+		share a guest post you've written
+		Post a quote by one of your heros
+		offer een discount or promo code
+		post a shocking statistic in your industry
+		share a motivatioinal quote
+		Post a selfie/picture of you working
+		Share a funny meme or photo
+		Post a ready for the weekend image
+"
 
 		];
 	}
